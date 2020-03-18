b434c864203d515c4cdb135954d7d894abf82678
Bennett Lawrenz <bennettlawrenz@gmail.com>
2020-03-17 21:50:03 -0700

    Updates

---
+++ precache-manifest.d3d325fcc68fff11a0283bce4035e01c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53d742d14179e3df72fa33a1200ad5fd",
+    "url": "/ReactNoteTakerApp/index.html"
+  },
+  {
+    "revision": "8c3c5fe0701008f45a76",
+    "url": "/ReactNoteTakerApp/static/css/main.f350f950.chunk.css"
+  },
+  {
+    "revision": "9edf7bb6cdfc1af07b76",
+    "url": "/ReactNoteTakerApp/static/js/2.9bbae32a.chunk.js"
+  },
+  {
+    "revision": "1206fbd8a34c9471eccff7edf5639e40",
+    "url": "/ReactNoteTakerApp/static/js/2.9bbae32a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c3c5fe0701008f45a76",
+    "url": "/ReactNoteTakerApp/static/js/main.e8856ba3.chunk.js"
+  },
+  {
+    "revision": "9ba4b231ff8b376fcf57",
+    "url": "/ReactNoteTakerApp/static/js/runtime-main.95f175e5.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ReactNoteTakerApp/static/media/logo.ee7cd8ed.svg"
+  }
+]);
